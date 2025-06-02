6422327cd5737ee41f71adbf142c2ce8f5debb4e
Adrián <adriangonzalodev@gmail.com>
2025-06-02 05:23:06 +0200

    fix footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,15 +1,25 @@
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="bg-[var(--footer)] w-full border-t-4 border-[var(--footerBorder)] text-white">
       <div className="flex flex-col justify-between min-h-[200px] ">
-        <div className="text-sm">
-          <p>
-            Junto al Puente Romano, Calle Camping, 1, 37657 Sotoserrano,
-            Salamanca
-          </p>
-          <p>josega1669@gmail.com</p>
-          <p>923 16 11 04</p>
-          <p>653 06 81 00</p>
+        <div className="text-sm flex justify-center space-y-1 ">
+          <div className=" w-130 flex flex-col gap-2 mt-2">
+            <p className="flex items-center gap-4">
+              <FaMapMarkerAlt /> Junto al Puente Romano, Calle Camping, 1, 37657
+              Sotoserrano, Salamanca
+            </p>
+            <p className="flex items-center gap-4">
+              <FaEnvelope /> josega1669@gmail.com
+            </p>
+            <p className="flex items-center gap-4">
+              <FaPhoneAlt /> Fijo: 923 16 11 04
+            </p>
+            <p className="flex items-center gap-4">
+              <FaPhoneAlt /> Movil: 653 06 81 00
+            </p>
+          </div>
         </div>
 
         {/* Parte inferior fija: políticas */}
